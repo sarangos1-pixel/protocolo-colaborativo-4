b4db4d88ac87fd653bb6b1b5ac4851e1d0fd8cc3
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:42:07 -0500

    clase Transporte ejercicio 3

---
+++ src/Taller17/Transporte.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller17;
+
+/**
+ *
+ * @author Sebastian
+ */
+public abstract class Transporte {
+    public abstract void mover();
+}
