903abbf36ec7188e8e87f4be946a682043a17077
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:39:54 -0500

    clase figura circula  ejercicio 1

---
+++ src/Taller17/Circulo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller17;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Circulo extends Figura {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+}
+
