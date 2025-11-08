6f1a5a719a0a5f98da9a925dc7400b9b3311e8cb
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:37:09 -0500

    clase mensajero mensajeSMS  ejercicio 3

---
+++ src/taller16/MensajeSMS.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller16;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class MensajeSMS {
+    public class MensajeroSMS extends Mensajero {
+    @Override
+    public void enviarMensaje(String mensaje) {
+        System.out.println("Enviando SMS: " + mensaje);
+    }
+}
+}
