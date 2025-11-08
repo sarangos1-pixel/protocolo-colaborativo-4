2bb5416efdb648026b45a00678122d16ecc8f7b6
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:29:33 -0500

    clase Descuento ejercicio 1

---
+++ src/taller16/Descuento.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller16;
+
+/**
+ *
+ * @author Sebastian
+ */
+public abstract class Descuento {
+    public abstract double descuento(double precioOriginal);
+}
