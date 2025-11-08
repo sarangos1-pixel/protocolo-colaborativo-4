fff525282f09ef34c7f429412c4f084d5caab5f8
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:40:09 -0500

    clase figura rectangulo  ejercicio 1

---
+++ src/Taller17/Rectangulo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller17;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Rectangulo extends Figura {
+    private double ancho;
+    private double alto;
+
+    public Rectangulo(double ancho, double alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    @Override
+    public double calcularArea() {
+        return ancho * alto;
+    }
+}
+
