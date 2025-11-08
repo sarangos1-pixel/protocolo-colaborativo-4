cdc0eebb60231787f0b84c410f763a06dea23ad0
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:39:36 -0500

    clase figura  ejercicio 1

---
+++ src/Taller17/Figura.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller17;
+
+/**
+ *
+ * @author Sebastian
+ */
+public abstract class Figura {
+    public abstract double calcularArea();
+}
+
