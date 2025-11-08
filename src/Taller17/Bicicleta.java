3d1c1fe13609c9b0db972cbce621a19c8ceabd12
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:42:38 -0500

    clase Transporte Bicicleta ejercicio 3

---
+++ src/Taller17/Bicicleta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller17;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Bicicleta extends Transporte {
+    @Override
+    public void mover() {
+        System.out.println("La bicicleta se está moviendo.");
+    }
+}
