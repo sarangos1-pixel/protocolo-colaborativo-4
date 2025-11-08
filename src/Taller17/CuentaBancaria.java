4ca5fae5bbe89618cf88bcbb60410559e346070a
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:41:08 -0500

    clase CuentaBancaria  ejercicio 2

---
+++ src/Taller17/CuentaBancaria.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller17;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class CuentaBancaria {
+    protected double saldo;
+
+    public CuentaBancaria(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double cantidad) {
+        saldo += cantidad;
+    }
+
+    public void retirar(double cantidad) {
+        if (cantidad <= saldo) {
+            saldo -= cantidad;
+        } else {
+            throw new IllegalArgumentException("Saldo insuficiente");
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
