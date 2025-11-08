a2df7a5e0f85402c4ecb1064c5acc6e976c7d9db
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:36:28 -0500

    clase Precio final  ejercicio 1

---
+++ src/taller16/PrecioFInal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller16;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class PrecioFInal {
+     public double CalcularPrecioFinal(Descuento descuentoF, double precioOriginal) {
+        return descuentoF.descuento(precioOriginal);
+    }
+}
+
