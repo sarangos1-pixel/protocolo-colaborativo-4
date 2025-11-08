8fcd0edfe88d5c992a3e0f5852e98eb9ef1a4431
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:37:41 -0500

    clase mensajero gestorMensajes  ejercicio 3

---
+++ src/taller16/GestorMensajes.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller16;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class GestorMensajes {
+    public void enviarMensaje(Mensajero canal, String mensaje) {
+        canal.enviarMensaje(mensaje);
+    }
+}
