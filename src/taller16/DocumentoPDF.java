6933e9a6fe1ed9f437a5ecb1773b678a20fb796f
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 22:30:36 -0500

    clase Documento DocumentoPDF ejercicio 1

---
+++ src/taller16/DocumentoPDF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller16;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class DocumentoPDF extends Documento {
+    @Override
+    public void exportar(String contenido) {
+        System.out.println("Exportando contenido a PDF: " + contenido);
+    }
+}
